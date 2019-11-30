bbd2bf1ee119e0759f50463e186ad31970aede7a
shaden333 <55825584+shaden333@users.noreply.github.com>
2019-11-30 15:26:08 +0300

    Merge branch 'master' of C:\Users\dell\Documents\GitHub\Hackwati-444 with conflicts.

---
+++ app/src/main/java/sa/ksu/swe444/hackwati/about_Us.java
@@ -0,0 +1,31 @@
+package sa.ksu.swe444.hackwati;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class about_Us extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about__us);
+
+        Button buttonContact = findViewById(R.id.contact_button;
+        buttonContact.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+
+                Intent intent = new Intent(this, ConcatUsActivity.class);
+                startActivity(intent);
+
+
+
+
+            }
+        });
+    }
+}
